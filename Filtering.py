2ae88176e2d6401556729b15a5a645cf68001e53
manasses.silva <manasses3mm@gmail.com>
2021-06-02 11:21:38 -0300

    commented functions

--- Filtering.py
+++ Filtering.py
@@ -15,6 +15,15 @@ class Filtering():
         pass
     
     def opening(self, image, kernel, iterations):
+        """
+        It do an opening morphological operation in an image/frame
+        
+        :param image: an image array
+        :param kernel: an interger value from 1 to 4 corresponding to one of the constant kernels will apply on the image/frame
+        :param iterations: an interger value to determine how many iterations will apply on the image/frame
+        
+        :return: an image/frame with the opening morphological operation applied
+        """
         if kernel == 1:
             return cv2.morphologyEx(image, cv2.MORPH_OPEN, kernel= self.KERNEL_1, iterations=iterations)
         elif kernel == 2:
@@ -25,6 +34,15 @@ class Filtering():
             return cv2.morphologyEx(image, cv2.MORPH_OPEN, kernel= self.KERNEL_4, iterations=iterations)
     
     def closing(self, image, kernel, iterations):
+        """
+        It do an closing morphological operation in an image/frame
+        
+        :param image: an image array
+        :param kernel: an interger value from 1 to 4 corresponding to one of the constant kernels will apply on the image/frame
+        :param iterations: an interger value to determine how many iterations will apply on the image/frame
+        
+        :return: an image/frame with the closing morphological operation applied
+        """
         if kernel == 1:
             return cv2.morphologyEx(image, cv2.MORPH_CLOSE, kernel= self.KERNEL_1, iterations=iterations)
         elif kernel == 2:
@@ -35,6 +53,15 @@ class Filtering():
             return cv2.morphologyEx(image, cv2.MORPH_CLOSE, kernel= self.KERNEL_4, iterations=iterations)
     
     def dilating(self, image, kernel, iterations):
+        """
+        It do an dilating morphological operation in an image/frame
+        
+        :param image: an image array
+        :param kernel: an interger value from 1 to 4 corresponding to one of the constant kernels will apply on the image/frame
+        :param iterations: an interger value to determine how many iterations will apply on the image/frame
+        
+        :return: an image/frame with the dilating morphological operation applied
+        """
         if kernel == 1:
             return cv2.morphologyEx(image, cv2.MORPH_DILATE, kernel= self.KERNEL_1, iterations=iterations)
         elif kernel == 2:
@@ -45,6 +72,15 @@ class Filtering():
             return cv2.morphologyEx(image, cv2.MORPH_DILATE, kernel= self.KERNEL_4, iterations=iterations)
     
     def eroding(self, image, kernel, iterations):
+        """
+        It do an eroding morphological operation in an image/frame
+        
+        :param image: an image array
+        :param kernel: an interger value from 1 to 4 corresponding to one of the constant kernels will apply on the image/frame
+        :param iterations: an interger value to determine how many iterations will apply on the image/frame
+        
+        :return: an image/frame with the eroding morphological operation applied
+        """
         if kernel == 1:
             return cv2.morphologyEx(image, cv2.MORPH_ERODE, kernel= self.KERNEL_1, iterations=iterations)
         elif kernel == 2:
@@ -55,6 +91,16 @@ class Filtering():
             return cv2.morphologyEx(image, cv2.MORPH_ERODE, kernel= self.KERNEL_4, iterations=iterations)
     
     def fillCountours(self, image, contours, width, height):
+        """
+        It will fill the contours of the identified objects in the image/frame
+        
+        :param image: an image array
+        :param contours: a list of contours of each object in the image/frame
+        :param width: the width of the image/frame
+        :param height: the height of the image/frame
+        
+        :return: an image/frame with the filled contours of objects
+        """
         for i in range(len(contours)):
             area = cv2.contourArea(contours[i])
             areaImage = width*height
@@ -62,6 +108,17 @@ class Filtering():
                 cv2.drawContours(image, contours, i, (255,255,255), cv2.FILLED)
     
     def fillConvexHull(self, image, hull, contours, width, height):
+        """
+        It do an opening morphological operation in an image/frame
+        
+        :param image: an image array
+        :param hull: a list where identified hulls will be saved 
+        :param contours: a list of contours of each object in the image/frame
+        :param width: the width of the image/frame
+        :param height: the height of the image/frame
+        
+        :return: an image/frame with the filled hulls of objects
+        """
         for i in range(len(contours)):
             hull.append(cv2.convexHull(contours[i], False))
         for i in range(len(hull)):
