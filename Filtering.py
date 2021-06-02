5bf3673cbc92457375f64461846ec643d774b79e
manasses.silva <manasses3mm@gmail.com>
2021-06-02 11:00:57 -0300

    identifying, cropping and saving objects

--- Filtering.py
+++ Filtering.py
@@ -1,26 +1,72 @@
 import cv2
 import numpy as np
+from numpy.core.numeric import count_nonzero
+from numpy.lib.type_check import imag
 
 class Filtering():
     
-    KERNEL_OPENING = np.ones((3,3), np.uint8)
-    KERNEL_CLOSING = np.ones((3,3), np.uint8)
-    KERNEL_DILATING = np.ones((3,3), np.uint8)
-    KERNEL_ERODING = np.ones((2,2), np.uint8)
+    KERNEL_1 = np.ones((2,2), np.uint8)
+    KERNEL_2 = np.ones((3,3), np.uint8)
+    KERNEL_3 = np.ones((4,4), np.uint8)
+    KERNEL_4 = np.ones((5,5), np.uint8)
     
     
     def __init__(self):
         pass
     
-    def opening(self, image):
-        return cv2.morphologyEx(image, cv2.MORPH_OPEN, kernel= self.KERNEL_OPENING, iterations=2)
+    def opening(self, image, kernel, iterations):
+        if kernel == 1:
+            return cv2.morphologyEx(image, cv2.MORPH_OPEN, kernel= self.KERNEL_1, iterations=iterations)
+        elif kernel == 2:
+            return cv2.morphologyEx(image, cv2.MORPH_OPEN, kernel= self.KERNEL_2, iterations=iterations)
+        elif kernel == 3:
+            return cv2.morphologyEx(image, cv2.MORPH_OPEN, kernel= self.KERNEL_3, iterations=iterations)
+        elif kernel == 4:
+            return cv2.morphologyEx(image, cv2.MORPH_OPEN, kernel= self.KERNEL_4, iterations=iterations)
     
-    def closing(self, image):
-        return cv2.morphologyEx(image, cv2.MORPH_CLOSE, kernel= self.KERNEL_CLOSING, iterations=2)
+    def closing(self, image, kernel, iterations):
+        if kernel == 1:
+            return cv2.morphologyEx(image, cv2.MORPH_CLOSE, kernel= self.KERNEL_1, iterations=iterations)
+        elif kernel == 2:
+            return cv2.morphologyEx(image, cv2.MORPH_CLOSE, kernel= self.KERNEL_2, iterations=iterations)
+        elif kernel == 3:
+            return cv2.morphologyEx(image, cv2.MORPH_CLOSE, kernel= self.KERNEL_3, iterations=iterations)
+        elif kernel == 4:
+            return cv2.morphologyEx(image, cv2.MORPH_CLOSE, kernel= self.KERNEL_4, iterations=iterations)
     
-    def dilating(self, image):
-        return cv2.morphologyEx(image, cv2.MORPH_DILATE, kernel= self.KERNEL_DILATING, iterations=2)
+    def dilating(self, image, kernel, iterations):
+        if kernel == 1:
+            return cv2.morphologyEx(image, cv2.MORPH_DILATE, kernel= self.KERNEL_1, iterations=iterations)
+        elif kernel == 2:
+            return cv2.morphologyEx(image, cv2.MORPH_DILATE, kernel= self.KERNEL_2, iterations=iterations)
+        elif kernel == 3:
+            return cv2.morphologyEx(image, cv2.MORPH_DILATE, kernel= self.KERNEL_3, iterations=iterations)
+        elif kernel == 4:
+            return cv2.morphologyEx(image, cv2.MORPH_DILATE, kernel= self.KERNEL_4, iterations=iterations)
     
-    def eroding(self, image):
-        return cv2.morphologyEx(image, cv2.MORPH_ERODE, kernel= self.KERNEL_ERODING, iterations=2)
-  
+    def eroding(self, image, kernel, iterations):
+        if kernel == 1:
+            return cv2.morphologyEx(image, cv2.MORPH_ERODE, kernel= self.KERNEL_1, iterations=iterations)
+        elif kernel == 2:
+            return cv2.morphologyEx(image, cv2.MORPH_ERODE, kernel= self.KERNEL_2, iterations=iterations)
+        elif kernel == 3:
+            return cv2.morphologyEx(image, cv2.MORPH_ERODE, kernel= self.KERNEL_3, iterations=iterations)
+        elif kernel == 4:
+            return cv2.morphologyEx(image, cv2.MORPH_ERODE, kernel= self.KERNEL_4, iterations=iterations)
+    
+    def fillCountours(self, image, contours, width, height):
+        for i in range(len(contours)):
+            area = cv2.contourArea(contours[i])
+            areaImage = width*height
+            if area > (areaImage*0.001):
+                cv2.drawContours(image, contours, i, (255,255,255), cv2.FILLED)
+    
+    def fillConvexHull(self, image, hull, contours, width, height):
+        for i in range(len(contours)):
+            hull.append(cv2.convexHull(contours[i], False))
+        for i in range(len(hull)):
+            area = cv2.contourArea(contours[i])
+            areaImage = width*height
+            if area > (areaImage*0.001):
+                cv2.drawContours(image, hull, i, (255,255,255), cv2.FILLED)
+        
--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from Filtering import Filtering as f
 
 
 def contours(frame):
-    _,contour, _ = cv2.findContours(frame, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    return contour
+    contour, _ = cv2.findContours(frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    return contour, _
 
 # Opening a video e checking if the video is valid
 cap = cv2.VideoCapture('STRANS/cam20.AVI')
@@ -13,53 +13,61 @@ if (cap.isOpened() == False):
     print("Unable to read camera feed")
 
 # Preparing the subtractors to identify the background
-subtractor1 = cv2.createBackgroundSubtractorMOG2(800, 16, True)
-subtractor2 = cv2.createBackgroundSubtractorKNN(dist2Threshold=1000, detectShadows=True)
+subtractor1 = cv2.createBackgroundSubtractorMOG2(800, 16, True) #it is not so good if compared with the o KNN method
+subtractor2 = cv2.createBackgroundSubtractorKNN(dist2Threshold=1000, detectShadows=False)
+
+frame_number = 0
 
 filter = f()
 
 while (True):
     ret, frame = cap.read()
+    object_number = 0
     frame2 = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    
     # Applying the subtractor and separetin background and foreground
     mask = subtractor2.apply(frame2)
-    mask11 = subtractor1.apply(frame2)
+    
+    #cv2.imshow("mask", mask)
+    
+    #mask11 = subtractor1.apply(frame2)   -- it is not being used because the other method preseted the best result if you compare with the other    
+    
     rett, mask = cv2.threshold(mask,127,255,cv2.THRESH_BINARY)
     cv2.imshow("mask", mask)
     
     hei, wid = frame2.shape
     # Taking the countours of the objects and drawning them on the frame
     mask1 = mask.copy()
-    mask1 = filter.eroding(mask1)
-    mask1 = filter.dilating(mask1)
+    mask1 = filter.opening(mask1, 1, 1)
+    mask1 = filter.dilating(mask1, 2, 2)
     
-    cv2.imshow("mask2", mask1)
-    c = contours(mask1)
-    for i in range(len(c)):
-        area = cv2.contourArea(c[i])
-        areaImage = wid*hei
-        if area > (areaImage*0.001):
-            cv2.drawContours(mask1, c, i, (255,255,255), cv2.FILLED)
-    
+    # Fill the contours and the convex hull
+    c, hierarchy = contours(mask1)
     hull = []
+    filter.fillCountours(mask1, c, wid, hei)
+    filter.fillConvexHull(mask1, hull, c, wid, hei)
+    c, hierarchy = contours(mask1)
+    hull = []
+    filter.fillCountours(mask1, c, wid, hei)
+    filter.fillConvexHull(mask1, hull, c, wid, hei)
+    cv2.imshow("filled convex hull", mask1)
     
-    for i in range(len(c)):
-        hull.append(cv2.convexHull(c[i]), False)
-        
     # Drawning a rectangule around the object
     mask2 = mask1.copy()
-    c = contours(mask2)
+    c, hierarchy = contours(mask2)
     for contour in c:
         (x, y, w, h) = cv2.boundingRect(contour)
         if cv2.contourArea(contour) < 700:
             continue
-        cv2.rectangle(frame, (x,y), (x+w, y+h), (0,0,0), 3)
-        cv2.putText(frame,'object',(x,y), cv2.FONT_HERSHEY_SIMPLEX, 1,(255,255,255),2)
-
+        crop = frame[y:y+h,x:x+w]
+        cv2.imwrite('CARS/'+'car'+str(frame_number)+str(object_number)+'.png', crop)
+        object_number += 1
+        cv2.rectangle(frame, (x,y), (x+w, y+h), (255,0,0), 2)
+        cv2.putText(frame,'object',(x,y), cv2.FONT_HERSHEY_SIMPLEX, 1,(255,255,255),1)
+    frame_number += 1
     # Display the resulting frame
     cv2.imshow("calssified", frame)  
-    cv2.waitKey(20)
+    cv2.waitKey(50)
+    
     # Press Q on keyboard to stop recording
     
 cap.release()
